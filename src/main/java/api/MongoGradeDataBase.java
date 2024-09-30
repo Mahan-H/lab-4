5e80ba2530d3e455d00d5c69c00f79f7daa54766
Mahan Hooshmandkhayat <mahan.hooshmandkhayat@mail.utoronto.ca>
2024-09-30 18:40:59 -0400

    fixed getMyTeam again

--- src/main/java/api/MongoGradeDataBase.java
+++ src/main/java/api/MongoGradeDataBase.java
@@ -257,13 +257,26 @@ public class MongoGradeDataBase implements GradeDataBase {
         try {
             response = client.newCall(request).execute();
             responseBody = new JSONObject(response.body().string());
+            if (responseBody.getInt(STATUS_CODE) == SUCCESS_CODE) {
+                final JSONObject team = responseBody.getJSONObject("team");
+                final JSONArray membersArray = team.getJSONArray("members");
+                final String[] members = new String[membersArray.length()];
+                for (int i = 0; i < membersArray.length(); i++) {
+                    members[i] = membersArray.getString(i);
+                }
+
+                return Team.builder()
+                        .name(team.getString(NAME))
+                        .members(members)
+                        .build();
+            }
+            else {
+                throw new RuntimeException(responseBody.getString(MESSAGE));
+            }
         }
         catch (IOException | JSONException event) {
             throw new RuntimeException(event);
         }
-        return Team.builder()
-                .name(responseBody.getString(NAME))
-                .members(new String[]{})
-                .build();
+
     }
 }
